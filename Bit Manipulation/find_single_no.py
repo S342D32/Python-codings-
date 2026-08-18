51aad09b2ea64b61b92c680a8c4dd971e7751630
s342d32 <sourajitnayak3471@gmail.com>
2026-08-18 22:29:42 +0530

    optimal bit count

--- Bit Manipulation/find_single_no.py
+++ Bit Manipulation/find_single_no.py
@@ -1,14 +1,20 @@
 arr = [1,2,4,6,3,8,6,9]
 
 
-
+# optimal
 def count(arr):
-    x =[]
-    hash_map={}
+    x = 0
     for num in arr:
-      hash_map[num] = hash_map.get(num,0) +1
-    for key in hash_map:
-      if hash_map[key] ==1:
-         x.append(key)
+        x = x ^ num
     return x        
+# Brute force
+# def count(arr):
+#     x =[]
+#     hash_map={}
+#     for num in arr:
+#       hash_map[num] = hash_map.get(num,0) +1
+#     for key in hash_map:
+#       if hash_map[key] ==1:
+#          x.append(key)
+#     return x        
 print(count(arr))
