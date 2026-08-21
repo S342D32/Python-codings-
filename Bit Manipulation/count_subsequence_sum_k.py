1cd49c51bacec9372c539dc2049483e06d6691b0
s342d32 <sourajitnayak3471@gmail.com>
2026-08-21 22:40:48 +0530

    count subsequense of sum k

---
+++ Bit Manipulation/count_subsequence_sum_k.py
@@ -0,0 +1,19 @@
+nums = [1,2,4,5,6,7]
+k = 8
+
+def backtrack(index,total):
+  if total == k:
+    return 1
+  elif total > k:
+    return 0
+  if index >= len(nums):
+    return 0
+  sum = total + nums[index]
+  pick = backtrack(index+1,sum)
+  sum = total
+  not_pick = backtrack(index+1,sum)
+  return pick + not_pick
+
+
+print(backtrack(0,0))
+
