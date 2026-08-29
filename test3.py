9e3b98135518beb9e9a443eedce322b4a762bc5f
s342d32 <sourajitnayak3471@gmail.com>
2026-08-29 23:10:32 +0530

    3 sum best

--- test3.py
+++ test3.py
@@ -1,13 +1,18 @@
-def three_sum_b(arr,tar):
-  my_set = set()
+def three_sum_best(arr,tar):
+  result = set()
   n=len(arr)
   for i in range(0,n):
+    seen= set()
     for j in range(i+1,n):
-      for k in range(j+1,n):
-        if arr[i]+ arr[j]+ arr[k] == tar:
-          temp =arr[i],arr[j],arr[k]
-          my_set.add(temp)
-  return [list(ans) for ans in my_set]
+      third =  tar -(arr[i]+arr[j])
+      if third in seen:
+          temp =[arr[i],arr[j],third]
+          temp.sort()
+          result.add(tuple(temp))
+      seen.add(arr[j])
+
+  return [list(ans) for ans in result]
 
 
-print(three_sum_brute([1,2,3,4,5,6,7,8],10))
+print(three_sum_best([1,2,3,4,5,6,7,8],10))
+
