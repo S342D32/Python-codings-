4bd389c4da6ecd541da7c74791c1b3a08a6f94fa
s342d32 <sourajitnayak3471@gmail.com>
2026-08-29 23:33:40 +0530

    three sum optimal

--- test3.py
+++ test3.py
@@ -1,18 +1,32 @@
-def three_sum_best(arr,tar):
-  result = set()
-  n=len(arr)
-  for i in range(0,n):
-    seen= set()
-    for j in range(i+1,n):
-      third =  tar -(arr[i]+arr[j])
-      if third in seen:
-          temp =[arr[i],arr[j],third]
-          temp.sort()
-          result.add(tuple(temp))
-      seen.add(arr[j])
+def three_sum_optimal(arr, tar):
+    arr.sort()
+    n = len(arr)
+    ans = []
 
-  return [list(ans) for ans in result]
+    for i in range(n):
+        if i > 0 and arr[i] == arr[i-1]:
+            continue
 
+        j = i + 1
+        k = n - 1
 
-print(three_sum_best([1,2,3,4,5,6,7,8],10))
+        while j < k:
+            total_sum = arr[i] + arr[j] + arr[k]
 
+            if total_sum < tar:
+                j += 1
+            elif total_sum > tar:
+                k -= 1
+            else:
+                ans.append([arr[i], arr[j], arr[k]])
+                j += 1
+                k -= 1
+
+                while j < k and arr[j] == arr[j-1]:
+                    j += 1
+                while j < k and arr[k] == arr[k+1]:
+                    k -= 1
+
+    return ans
+
+print(three_sum_optimal([1,2,3,4,5,6,7,8], 10))
