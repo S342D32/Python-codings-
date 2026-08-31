9ed8dd6174800a6880af8e4c469f0a1f62620ea9
s342d32 <sourajitnayak3471@gmail.com>
2026-08-31 21:20:03 +0530

    ceil-floor

---
+++ YouTube(Code & Debug)/ceil_floor.py
@@ -0,0 +1,16 @@
+def ceil_floor(arr, tar):
+    arr.sort()
+
+    floor_val = None
+    ceil_val = None
+
+    for num in arr:
+        if num <= tar:
+            floor_val = num
+
+        if num >= tar and ceil_val is None:
+            ceil_val = num
+
+    return floor_val, ceil_val
+
+print(ceil_floor([2,3,4,5,7,3], 4))
