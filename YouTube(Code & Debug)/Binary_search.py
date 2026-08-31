55f345941bb6e05e98415968c5a2a340c05ef9a6
s342d32 <sourajitnayak3471@gmail.com>
2026-08-31 20:35:40 +0530

    binary search recursion

--- YouTube(Code & Debug)/Binary_search.py
+++ YouTube(Code & Debug)/Binary_search.py
@@ -1,19 +1,37 @@
-def binary(arr,tar):
+def binary_recursion(arr,tar,low,high):
   arr.sort()
-  n = len(arr)
-  low = 0
-  high = n-1
   while low<=high:
     mid = (low+high)//2
     if arr[mid] < tar:
-      low=mid+1
+      return binary_recursion(arr,tar,mid+1,high)
     elif arr[mid] > tar:
-      high=mid-1
+      return binary_recursion(arr,tar,low,mid-1)
     else:
       return mid
   return -1
 
-print(binary([2,4,5,6,3,7],3))
+arr=[2,4,5,6,3,7]
+n = len(arr)
+low = 0
+high = n-1
+print(binary_recursion(arr,3,low,high))
+
+# def binary(arr,tar):
+#   arr.sort()
+#   n = len(arr)
+#   low = 0
+#   high = n-1
+#   while low<=high:
+#     mid = (low+high)//2
+#     if arr[mid] < tar:
+#       low=mid+1
+#     elif arr[mid] > tar:
+#       high=mid-1
+#     else:
+#       return mid
+#   return -1
+
+# print(binary([2,4,5,6,3,7],3))
 
 
 
