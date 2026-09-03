edaae014b1aa5b6777d1f2d8a9187786ee94423d
s342d32 <sourajitnayak3471@gmail.com>
2026-09-03 22:40:58 +0530

    dleted

--- YouTube(Code & Debug)/combination_sum.py
+++ YouTube(Code & Debug)/combination_sum.py
@@ -1,7 +1,24 @@
-def solve(index,arr,target,total,result):
-  if index >=len(arr):
-    result.append(arr[index])
-    return
+def solve(index,total,subset,nums,target,result):
   if total == target:
-    return 
-  
+    result.append(subset.copy())
+    return
+  elif total>target:
+    return
+  if index>=len(nums):
+    return
+  sum = total+nums[index]
+  subset.append(nums[index])
+  solve(index,sum,subset,nums,target,result)
+  sum = total
+  subset.pop()
+  solve(index+1,sum,subset,nums,target,result)
+
+def combination(nums,target):
+  result=[]
+  solve(0,0,[],nums,target,result)
+  return result
+
+print(combination([2,3,4,5,6],7))
+
+# TC=O(2^N)
+# SC= O(N)
