d3c7ddb4654c96bbd41b23a51142a9c13a9d67c1
s342d32 <sourajitnayak3471@gmail.com>
2026-09-07 23:43:16 +0530

    time complexity

--- YouTube(Code & Debug)/subset_sum_I.py
+++ YouTube(Code & Debug)/subset_sum_I.py
@@ -14,3 +14,13 @@ print(subset_sum(0,[1,2,3],[],[]))
 
 
 
+# Time Complexity: O(2ⁿ × n)
+
+# Why?
+
+# There are 2 choices for each element: include or exclude.
+
+# With n elements, the recursion generates 2ⁿ subsets.
+
+# For every subset, subset.copy() takes up to O(n) time.
+# SC=O(N)
