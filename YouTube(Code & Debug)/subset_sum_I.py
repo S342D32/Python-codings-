68cc7b3dbf991cf940684ec2f454095fefa0b252
s342d32 <sourajitnayak3471@gmail.com>
2026-09-07 23:34:46 +0530

    subset sum optimal

--- YouTube(Code & Debug)/subset_sum_I.py
+++ YouTube(Code & Debug)/subset_sum_I.py
@@ -1,17 +1,15 @@
-# brute force
 def subset_sum(index,arr,subset,result):
   n =len(arr)
   if index == n:
     result.append(subset.copy())
-  if index <n:
+  if index > n:
     return
-  for i in range(index,n):
-      subset.append(arr[i]) 
-      subset_sum(i+1,arr,subset,result)
-      subset.pop()
+  subset.append(arr[index]) 
+  subset_sum(index+1,arr,subset,result)
+  subset.pop()
   return subset_sum
 
-print(subset_sum)
+print(subset_sum(0,[2,3,4,5,6],[],[]))
 
 
 
