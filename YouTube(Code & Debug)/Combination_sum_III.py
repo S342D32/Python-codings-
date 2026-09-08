9a10993e0a425d55b408881a88cb9c4309f0547d
s342d32 <sourajitnayak3471@gmail.com>
2026-09-08 23:07:39 +0530

    ADDED PHOEN NUMBER COMBINATIN

--- YouTube(Code & Debug)/Combination_sum_III.py
+++ YouTube(Code & Debug)/Combination_sum_III.py
@@ -5,9 +5,9 @@ def solve(index,total,subset,result,arr,target,k):
   if total > target or len(subset)>k:
     return
 
-  for i in range(index,len(arr)+1):
-      sum = total + i
-      subset.append(i)
+  for i in range(index,len(arr)):
+      sum = total + arr[i]
+      subset.append(arr[i])
       solve(i + 1,sum,subset,result,arr,target,k)
       subset.pop()
   return result
@@ -15,4 +15,34 @@ def solve(index,total,subset,result,arr,target,k):
 print(solve(0,0,[],[],[2,3,4,5,6,7],7,2))
 
 
-# TC=O()
+# Time Complexity (TC)
+
+# Let: n = number of elements in arr
+
+# k = size of each combination
+
+# TC: O((nCk)×k)
+
+# Reason:
+
+# The algorithm explores all combinations of k elements from n elements.
+
+# There are (
+# k
+# n
+# 	​
+
+# ) such combinations.
+
+# Copying each valid subset (subset.copy()) takes O(k).
+
+# Space Complexity (SC)
+
+# Auxiliary Space: O(k) (recursion stack + current subset).
+
+# Output Space: O((
+# k
+# n
+# 	​
+
+# )×k) (if storing all answers).
